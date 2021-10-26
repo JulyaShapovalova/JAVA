329d9db02f5bc1cddfcee40444b4184c074918ff
JulyaLemaeva <85121501+JulyaShapovalova@users.noreply.github.com>
2021-10-26 17:59:05 +0300

    Update MyFirstProgram.java
    ASAs

--- src/main/java/ru/JAVAlearning/sandbox/MyFirstProgram.java
+++ src/main/java/ru/JAVAlearning/sandbox/MyFirstProgram.java
@@ -3,7 +3,9 @@ package ru.JAVAlearning.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-	  System.out.println("Hello, world!");
+		System.out.println("Hello, world!");
+
+		System.out.println(2+2);
 	}
 
 }
